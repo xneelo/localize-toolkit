4313b0fe422ae53f9299ee22c618fc02e470e42f
liamross <liamross.code@gmail.com>
2018-12-03 14:07:26 -0800

    Updated naming

--- src/LocalizeProvider.tsx
+++ src/LocalizeProvider.tsx
@@ -32,7 +32,7 @@ export const LocalizeProvider: React.SFC<ILocalizeProviderProps> = ({
   const [currentLanguage, setCurrentLanguage] = React.useState<string>('');
 
   // The cache of phrases mapped to their language.
-  const [storedPhrases, setStoredPhrases] = React.useState<{ [language: string]: IPhrases }>({});
+  const [cachedPhrases, setCachedPhrases] = React.useState<{ [language: string]: IPhrases }>({});
 
   // The current status of fetching languages from getPhrases.
   const [status, setStatus] = React.useState<ProviderStatus>(ProviderStatus.Loading);
@@ -56,10 +56,10 @@ export const LocalizeProvider: React.SFC<ILocalizeProviderProps> = ({
         newPhrases = await getPhrases(language);
       } else if (!isLanguageCached(language)) {
         throw new Error(
-          `No language object provided, language ${language} is not cached, and no getPhrases function is provided.`,
+          `No language object provided, language ${language} is not cached, and the getPhrases prop is not provided.`,
         );
       } else {
-        newPhrases = storedPhrases[language];
+        newPhrases = cachedPhrases[language];
       }
 
       // Update the current language.
@@ -75,7 +75,7 @@ export const LocalizeProvider: React.SFC<ILocalizeProviderProps> = ({
       // Store new language object in the state if new object exists and no
       // cache is false.
       if (newPhrases && !noCache) {
-        setStoredPhrases(previousState => ({
+        setCachedPhrases(previousState => ({
           ...previousState,
           [language]: newPhrases as IPhrases,
         }));
@@ -87,17 +87,17 @@ export const LocalizeProvider: React.SFC<ILocalizeProviderProps> = ({
     setStatus(ProviderStatus.Loaded);
   };
 
-  const isLanguageCached = (language: string) => language in storedPhrases;
+  const isLanguageCached = (language: string) => language in cachedPhrases;
 
   const clearCache = (language?: string) => {
     if (language) {
-      if (language in storedPhrases) {
-        const newState = { ...storedPhrases };
+      if (language in cachedPhrases) {
+        const newState = { ...cachedPhrases };
         delete newState[language];
-        setStoredPhrases(newState);
+        setCachedPhrases(newState);
       }
     } else {
-      setStoredPhrases({});
+      setCachedPhrases({});
     }
   };
 
