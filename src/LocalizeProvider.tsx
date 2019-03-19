6936de09bf35fcf4e9d57782994f73056cee0555
liamross <liamross.code@gmail.com>
2019-03-19 18:21:53 +0200

    Memoization of internal functions

--- src/LocalizeProvider.tsx
+++ src/LocalizeProvider.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useCallback, useMemo, useState} from 'react';
 import {LocalizeContext, LocalizeContextValue, localizePolyglot} from './Instances';
 
 /**
@@ -34,74 +34,82 @@ export const LocalizeProvider: React.FC<LocalizeProviderProps> = ({children, get
   // The current error status of fetching languages.
   const [error, setError] = useState<Error | null>(null);
 
-  const isLanguageCached = (language: string) => language in cachedPhrases;
+  const isLanguageCached = useCallback((language: string) => language in cachedPhrases, [cachedPhrases]);
 
-  const setLanguage = async (language: string, phrases?: Phrases) => {
-    try {
-      // New language object will be either given object, fetched object, or
-      // null. If new language object is not defined at this step, and there is
-      // no cache for the language, then this statement will throw and the
-      // language will not be set (no language object available).
-      let newPhrases: Phrases | null = null;
+  const setLanguage = useCallback(
+    async (language: string, phrases?: Phrases) => {
+      try {
+        // New language object will be either given object, fetched object, or
+        // null. If new language object is not defined at this step, and there is
+        // no cache for the language, then this statement will throw and the
+        // language will not be set (no language object available).
+        let newPhrases: Phrases | null = null;
 
-      if (phrases) {
-        newPhrases = phrases;
-      } else if (!isLanguageCached(language) && getPhrases) {
-        setLoading(true);
-        newPhrases = await getPhrases(language);
-      } else if (!isLanguageCached(language)) {
-        throw new TypeError(
-          `No language object provided, language ${language} is not cached,` +
-            ` and the getPhrases prop is not provided.`,
-        );
+        if (phrases) {
+          newPhrases = phrases;
+        } else if (!isLanguageCached(language) && getPhrases) {
+          setLoading(true);
+          newPhrases = await getPhrases(language);
+        } else if (!isLanguageCached(language)) {
+          throw new TypeError(
+            `No language object provided, language ${language} is not cached,` +
+              ` and the getPhrases prop is not provided.`,
+          );
+        } else {
+          newPhrases = cachedPhrases[language];
+        }
+
+        // Update the current language.
+        setCurrentLanguage(language);
+
+        // Extend polyglot to use new language object or cached, and set locale.
+        localizePolyglot.replace(newPhrases);
+        localizePolyglot.locale(language);
+
+        // Store new language object in the state if new object exists and no
+        // cache is false.
+        if (newPhrases && !noCache) {
+          setCachedPhrases(previousState => ({
+            ...previousState,
+            [language]: newPhrases as Phrases,
+          }));
+        }
+      } catch (error) {
+        setError(error);
+      }
+
+      setLoading(false);
+    },
+    [cachedPhrases, getPhrases, isLanguageCached, noCache],
+  );
+
+  const clearCache = useCallback(
+    (language?: string) => {
+      if (language) {
+        if (language in cachedPhrases) {
+          const newState = {...cachedPhrases};
+          delete newState[language];
+          setCachedPhrases(newState);
+        }
       } else {
-        newPhrases = cachedPhrases[language];
+        setCachedPhrases({});
       }
+    },
+    [cachedPhrases],
+  );
 
-      // Update the current language.
-      setCurrentLanguage(language);
-
-      // Extend polyglot to use new language object or cached, and set locale.
-      localizePolyglot.replace(newPhrases);
-      localizePolyglot.locale(language);
-
-      // Store new language object in the state if new object exists and no
-      // cache is false.
-      if (newPhrases && !noCache) {
-        setCachedPhrases(previousState => ({
-          ...previousState,
-          [language]: newPhrases as Phrases,
-        }));
-      }
-    } catch (error) {
-      setError(error);
-    }
-
-    setLoading(false);
-  };
-
-  const clearCache = (language?: string) => {
-    if (language) {
-      if (language in cachedPhrases) {
-        const newState = {...cachedPhrases};
-        delete newState[language];
-        setCachedPhrases(newState);
-      }
-    } else {
-      setCachedPhrases({});
-    }
-  };
-
-  // Provider value
-  const value: LocalizeContextValue = {
-    loading,
-    error,
-    currentLanguage,
-    setLanguage,
-    isLanguageCached,
-    clearCache,
-    t: (phrase, options?) => localizePolyglot.t(phrase, options),
-  };
+  const value = useMemo<LocalizeContextValue>(
+    () => ({
+      loading,
+      error,
+      currentLanguage,
+      setLanguage,
+      isLanguageCached,
+      clearCache,
+      t: (phrase, options?) => localizePolyglot.t(phrase, options),
+    }),
+    [loading, error, currentLanguage, setLanguage, isLanguageCached, clearCache],
+  );
 
   return <LocalizeContext.Provider value={value}>{children}</LocalizeContext.Provider>;
 };
