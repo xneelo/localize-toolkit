1234877e53fd135f3e1dc29b7ea347bbfa3bc7fa
liamross <liamross.code@gmail.com>
2019-06-25 14:41:22 -0700

    Added information to useLocalize hook

--- src/useLocalize.ts
+++ src/useLocalize.ts
@@ -1,7 +1,11 @@
 import {useContext} from 'react';
 import {LocalizeContext} from './Instances';
+import {LocalizeContextValue} from './types';
 
-export const useLocalize = () => {
+/**
+ * A hook that returns the localize context value.
+ */
+export const useLocalize = (): LocalizeContextValue => {
   const localize = useContext(LocalizeContext);
   return localize;
 };
